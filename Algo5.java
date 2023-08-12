4a506e4970f4524710a4f686cbc766665db42b65
Pubudu Ariyawansha <pubuduariyawansa@gmail.com>
2023-08-12 21:21:20 +0530

    Complete Task-5

---
+++ Algo5.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Algo5 {
+
+    public static final Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        String text;
+        char [] arr={};
+        
+        do{
+            System.out.print("Enter a text : ");
+            text = scan.nextLine();
+            if(text.isEmpty()){
+                System.out.println("Enter Valid Input");
+                continue;
+            }
+
+            arr = text.toCharArray();
+            char [] temp = new char[arr.length];
+            for (int i = 0; i < arr.length; i++) {
+                temp[i]=arr[arr.length-1-i];
+            }
+            arr=temp;
+
+        } while(text.isEmpty());
+
+        String str = new String();
+        String value = str.valueOf(arr);
+
+        System.out.println(value);
+        
+    }
+}
