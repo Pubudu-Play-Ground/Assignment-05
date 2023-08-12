1efcb4ebfb98d0dff0369a3e0bd917b132601ac6
Pubudu Ariyawansha <pubuduariyawansa@gmail.com>
2023-08-12 20:53:13 +0530

    Complete Task-3

---
+++ Algo3.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Algo3 {
+    public static void main(String[] args) {
+        
+        int[] nums = {10, 20, 30, 40, 50};
+
+        int [] temp=new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            temp[i]=nums[nums.length-1-i];
+        }
+
+        nums=temp;
+        System.out.println(Arrays.toString(nums));
+    }
+}
