87d350b36925a2dc4eae5474578edbf08f731c3a
Pubudu Ariyawansha <pubuduariyawansa@gmail.com>
2023-08-13 11:54:57 +0530

    Complete Task-10

--- Algo10.java
+++ Algo10.java
@@ -1,5 +1,65 @@
+import java.util.Scanner;
+
 public class Algo10 {
+    private static final Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
-        
+
+        boolean isTrue;
+
+        do{
+
+            System.out.print("Enter your phone number : ");
+            String number = scan.nextLine();
+            isTrue=false;
+
+          
+            if (number.startsWith("0")){
+                if(number.length()==10){
+                    
+                    char[] arr = number.toCharArray();
+                    for (int i = 0; i < arr.length; i++) {
+                        if(Character.isDigit(arr[i])){
+                            continue;
+                        }else{
+                            System.out.println("Enter correct phone number");
+                            isTrue=true;
+                            break;
+                        }
+                    }
+
+                } else{
+                    System.out.println("Enter correct phone number");
+                    isTrue=true;
+                }
+            } else if (number.startsWith("+94")){
+                if(number.length()==12){
+                    
+                    char[] arr = number.toCharArray();
+                    for (int i = 1; i < arr.length; i++) {
+                        if(Character.isDigit(arr[i])){
+                            continue;
+                        }else{
+                            System.out.println("Enter correct phone number");
+                            isTrue=true;
+                            break;
+                        }
+                    }
+
+                } else{
+                    System.out.println("Enter correct phone number");
+                    isTrue=true;
+                }
+            } else if(number.isBlank()){
+                System.out.println("Telephone number can't be empty.");
+                isTrue=true;
+            } else{
+                System.out.println("Invalid input");
+                isTrue=true;
+            }
+
+        } while(isTrue);
+
+        System.out.println("The number is validated");
+            
     }
 }
