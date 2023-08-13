60f3b93d8a0d9180025438431bd5d559c888b40f
Pubudu Ariyawansha <pubuduariyawansa@gmail.com>
2023-08-13 12:50:51 +0530

    Complete Task-8

--- Algo8.java
+++ Algo8.java
@@ -58,8 +58,70 @@ public class Algo8 {
                 all=temp;
             }
         }
-        System.out.println("All array : "+Arrays.toString(all));
+        System.out.println("Union array : "+Arrays.toString(all));
+
 
         
+
+        if (numA.length == numB.length) {
+            int[] result = new int[numA.length];
+
+            for (int i = 0; i < numA.length; i++) {
+                result[i] = numA[i] / numB[i];
+            }
+
+            System.out.println("numA/numB : "+Arrays.toString(result));
+        } else {
+            System.out.println("Arrays have different lengths, cannot perform division.");
+        }
+
+       
+
+        if (numA.length == numB.length) {
+            int[] result = new int[numA.length];
+
+            for (int i = 0; i < numA.length; i++) {
+                result[i] = numB[i] / numA[i];
+            }
+
+            System.out.println("numB/numA : "+Arrays.toString(result));
+        } else {
+            System.out.println("Arrays have different lengths, cannot perform division.");
+        }
+
+        
+
+        int [] diff = new int[0];
+        loopdiff:
+        for (int i = 0; i < numA.length+numB.length; i++) {
+            if(i<numA.length){
+                for (int j = 0; j < numB.length; j++) {
+                    if(numA[i]==numB[j]){
+                        continue loopdiff;
+                    }
+                }
+                int [] temp = new int[diff.length+1];
+                for (int k = 0; k < diff.length; k++) {
+                    temp[k]=diff[k];
+                }
+                temp[temp.length-1]=numA[i];
+                diff=temp;
+
+            } else{
+                for (int j = 0; j < numA.length; j++) {
+                    if(numB[i-numA.length]==numA[j]){
+                        continue loopdiff;
+                    }
+                }
+                int [] temp = new int[diff.length+1];
+                for (int k = 0; k < diff.length; k++) {
+                    temp[k]=diff[k];
+                }
+                temp[temp.length-1]=numB[i-numA.length];
+                diff=temp;
+            }
+        }
+        System.out.println("Symmetric Difference : "+Arrays.toString(diff));
+
     }
 }
